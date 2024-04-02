0801b244d8b745800e85f76508058937fa167138
Pranjal Gupta <prgu8117@aesd-students.colorado.edu>
2024-04-02 14:21:53 +0000

    changed commit of A3

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 1a70bf1c052b126410e506410a60dc6966bc0014
+AESD_ASSIGNMENTS_VERSION = aba0a16ed2d6793fe99e717dbe947b977bdee33e
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 8 git contents
-AESDCHAR_VERSION = 1a70bf1c052b126410e506410a60dc6966bc0014
+AESDCHAR_VERSION = aba0a16ed2d6793fe99e717dbe947b977bdee33e
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
